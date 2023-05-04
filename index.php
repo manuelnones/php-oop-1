c9022f1f577d5cea011debbf4fa5cce8ed840592
manuelnones <manuelnones99@yahoo.com>
2023-05-04 10:50:21 +0200

    correct exercise

--- index.php
+++ index.php
@@ -7,19 +7,29 @@ class Movie
     public $genre;
     public $time;
 
-    function __construct(string $name, string $originalLanguage, string $genre, string $time)
+    function __construct(string $name, string $originalLanguage, string $genre, int $time)
     {
         $this->name = $name;
         $this->originalLanguage = $originalLanguage;
         $this->genre = $genre;
         $this->time = $time;
     }
+
+    public function getLenghtHours() {
+        $hoursLenght = floor($this->time / 60);
+
+        if($hoursLenght > 1) {
+            return $hoursLenght . " ore";
+        } else {
+            return "1 ora";
+        }
+    }
 }
 
-
-$interstellar = new Movie("Interstellar", "en", "fantascienza", "2h 49m");
-$theImitationGame = new Movie("The imitation game", "en", "biografico", "1h 53m");
-$avengers = new Movie("Averngers", "en", "fantasy", "2h 23m");
+$interstellar = new Movie("Interstellar", "en", "fantascienza", 149);
+$theImitationGame = new Movie("The imitation game", "en", "biografico", 92);
+$avengers = new Movie("Averngers", "en", "fantasy", 134);
+$tiPresentoJoeBlack = new Movie("Meet Joe Black", "en", "drammatico", 186);
 
 
 echo $interstellar->name;
@@ -28,7 +38,7 @@ echo $interstellar->originalLanguage;
 echo "<br>";
 echo $interstellar->genre;
 echo "<br>";
-echo $interstellar->time;
+echo $interstellar->getLenghtHours();
 echo "<br>";
 echo "<br>";
 
@@ -38,7 +48,7 @@ echo $theImitationGame->originalLanguage;
 echo "<br>";
 echo $theImitationGame->genre;
 echo "<br>";
-echo $theImitationGame->time;
+echo $theImitationGame->getLenghtHours();
 echo "<br>";
 echo "<br>";
 
@@ -48,4 +58,14 @@ echo $avengers->originalLanguage;
 echo "<br>";
 echo $avengers->genre;
 echo "<br>";
-echo $avengers->time;
+echo $avengers->getLenghtHours();
+echo "<br>";
+echo "<br>";
+
+echo $tiPresentoJoeBlack->name;
+echo "<br>";
+echo $tiPresentoJoeBlack->originalLanguage;
+echo "<br>";
+echo $tiPresentoJoeBlack->genre;
+echo "<br>";
+echo $tiPresentoJoeBlack->getLenghtHours();
